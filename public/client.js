ad6ff831e010511e8b3d5365643af6fbd0f6cdf7
retobe <retobeyt@gmail.com>
2023-09-20 17:45:48 -0400

    REGISTER FORM WORKS. LOGIN FORM DOESN'T (404)

--- public/client.js
+++ public/client.js
@@ -1,33 +1,61 @@
+// Registration
 document.getElementById("registrationForm").addEventListener("submit", async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const requestData = {
-      username: formData.get("username"),
-      email: formData.get("email"),
-      password: formData.get("password")
+        username: formData.get("username"),
+        email: formData.get("email"),
+        password: formData.get("password")
     };
-  
+
     try {
-      const response = await fetch("http://localhost:3000/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(requestData)
-      });
-  
-      if (response.status === 201) {
-        alert("User registered successfully!");
-        // Redirect or perform other actions as needed
-      } else if (response.status === 400) {
-        const data = await response.json();
-        alert(data.message); // Display the error message from the server
-      } else {
-        alert("Registration failed. Please try again.");
-      }
+        const response = await fetch(`http://localhost:3001/register`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(requestData)
+        });
+
+        if (response.status === 201) {
+            alert("User registered successfully!");
+            return window.location.replace("Login.htm");
+        } else if (response.status === 400) {
+            const data = await response.json();
+            alert(data.message); // Display the error message from the server
+        } else {
+            alert("Registration failed. Please try again.");
+        }
     } catch (error) {
-      console.error(error);
-      alert("An error occurred while processing your request.");
+        console.error(error);
+        alert("An error occurred while processing your request.");
     }
-  });
-  
+});
+
+// login form
+
+document.getElementById("loginForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+    try {
+        const response = await fetch(`http://localhost:3001/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(requestData)
+        });
+
+        if (response.status === 200) {
+            alert("User Logged In successfully!");
+            return window.location.replace("Home.htm");
+        } else if (response.status === 400) {
+            const data = await response.json();
+            alert(data.message); // Display the error message from the server
+        } else {
+            alert("Login failed. Please try again.");
+        }
+    } catch (error) {
+        console.error(error);
+        alert("An error occurred while processing your request.");
+    }
+});
--- server/server.js
+++ server/server.js
@@ -14,40 +14,62 @@ app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded form da
 
 // MongoDB connection setup
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('MongoDB connection error:', error);
-  });
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('MongoDB connection error:', error);
+    });
 
-// Define registration route
+// REGISTER FORM
 app.post('/register', async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
+    try {
+        const { username, email, password } = req.body;
 
-    // Check if the email already exists in the database
-    const existingUser = await User.findOne({ email });
+        // Check if the email already exists in the database
+        const existingUser = await User.exists({ name: username, email: email })
+        console.log(!!existingUser);
 
-    if (existingUser) {
-      return res.status(400).json({ message: 'Email already exists' });
+        if (!!existingUser === true) {
+            return res.status(400).json({ message: 'Email/Username already exists.' });
+        }
+
+        // Create a new user
+        const user = await User.create({ name: username, email: email, password: password });
+
+        // Save the user to the database
+        await user.save();
+        console.log(user);
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Registration failed' });
     }
+});
 
-    // Create a new user
-    const newUser = new User({ username, email, password });
 
-    // Save the user to the database
-    await newUser.save();
+//LOGIN FORM
+app.post('/login', async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const user = User.exists({ name: username, email: email, password: password });
+        console.log(!!user);
+        if (!!user === true) {
+            res.status(200).json({ message: 'Successfully Logged in.' });
+        } else {
+            res.status(400).json({ message: 'This doesnt exist' });
+        }
 
-    res.status(201).json({ message: 'User registered successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Registration failed' });
-  }
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Submission failed' });
+    }
 });
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
+
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
